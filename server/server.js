e8dae56424261515943d3d9d452c0a1f7abc2ea1
Muhammad Abdullah Warraich <muhammadabdullah2016pk@gmail.com>
2025-04-05 19:52:05 +0500

    set up login

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const LogController = require('./LogController');
+const PlayerController = require('./controllers/PlayerController');
+const { initializeDatabase } = require('./db');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
+initializeDatabase();
+
 // Route binding
-const logController = new LogController();
-app.use('/api/log', logController.router);
+const playerController = new PlayerController();
+app.use('/api/player', playerController.router);
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
