e39ad6d50b1c528ab5a4543fea0c9f63d014d91f
Muhammad Abdullah Warraich <muhammadabdullah2016pk@gmail.com>
2025-04-05 22:25:06 +0500

    add logout functionality

--- server/player_client.js
+++ server/player_client.js
@@ -1,8 +1,11 @@
-const BASE_URL = 'http://localhost:3000/api/player/login';
+const BASE_URL = 'http://localhost:3000/api/player';
+const LOGIN_URL = BASE_URL + "/login";
+const LOGOUT_URL = BASE_URL + "/logout";
+
 const axios = require('axios');
-async function tryLogin(creds) {
+async function sendRequest(creds, url) {
   try {
-    const res = await axios.post(BASE_URL, creds);
+    const res = await axios.post(url, creds);
     console.log(res.data);
   } catch (err) {
     //console.log("We got an error: ", err);
@@ -10,25 +13,17 @@ async function tryLogin(creds) {
   }
 }
 async function main() {
-  await tryLogin({
+  await sendRequest({
     "username": "what_a_name",
     "password": "what_a_password"
-  });
-  await tryLogin({
+  }, LOGIN_URL);
+  await sendRequest({
     "username": "what_a_name",
     "password": "what_a_password"
-  });
-  await tryLogin({
+  }, LOGIN_URL);
+  await sendRequest({
     "username": "what_a_name",
     "password": "what_a_password"
-  });
-  await tryLogin({
-    "username": "what_a_name",
-    "password": "what_a_wrong_password"
-  });
-  await tryLogin({
-    "username": "what_not aname",
-    "password": "what_a_password"
-  });
+  }, LOGOUT_URL);
 }
 main();
