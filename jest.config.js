e9e1aa00ef32bbd5477f7815bacda33ad75fb227
Muhammad Abdullah Warraich <muhammadabdullah2016pk@gmail.com>
2025-05-08 11:20:04 +0500

    fix: setup some jest stuff

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,20 @@
 module.exports = {
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["@testing-library/jest-dom/extend-expect"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
+    // Handle CSS imports
+    "^.+\\.(css|sass|scss)$": "identity-obj-proxy",
+    // Handle module aliases (if you have them in your tsconfig.json)
+    "^@/components/(.*)$": "<rootDir>/components/$1",
   },
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+  transform: {
+    // Use babel-jest to transpile tests with next/babel preset
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: [
+    "/node_modules/",
+    "^.+\\.module\\.(css|sass|scss)$",
+  ],
 }
-
